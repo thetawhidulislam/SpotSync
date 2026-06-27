9e198b358b5980738cff7a73df7c27175d5662be
unknown <designwithtawhid@gmail.com>
2026-06-28 03:48:20 +0600

    updated server route

--- internal/server/http.go
+++ internal/server/http.go
@@ -7,9 +7,9 @@ import (
 	"gorm.io/gorm"
 	"net/http"
 	"spotsync/internal/config"
+	"spotsync/internal/domain/reservation"
 	"spotsync/internal/domain/user"
 	"spotsync/internal/domain/zone"
-	"spotsync/internal/domain/reservation"
 )
 
 type CustomValidator struct {
@@ -26,7 +26,7 @@ func Start(db *gorm.DB, cfg *config.Config) {
 	e := echo.New()
 	e.Validator = &CustomValidator{validator: validator.New()}
 
-	e.GET("/health", func(c *echo.Context) error {
+	e.GET("", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{
 			"message": "Server is running",
 		})
